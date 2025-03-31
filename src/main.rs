c157604b5525421304a0408d7f2029f37d68be34
goat2130 <yagisawahodaka@team-lab.com>
2025-03-31 19:09:57 +0900

    update: sec2

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,26 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[unsafe(no_mangle)] // この関数の名前修飾をしない
 pub extern "C" fn _start() -> ! {
     // リンカはデフォルトで `_start` という名前の関数を探すので、
     // この関数がエントリポイントとなる
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // VGA バッファは 80x25 のテキストモードのため、2バイトごとに
+            // 1バイト目が文字、2バイト目が属性を表す
+            // 0xb8000 は VGA バッファのアドレス
+            // 0xb8000 + i * 2 は i 番目の文字のアドレス
+            // 0xb8000 + i * 2 + 1 は i 番目の属性のアドレス
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
