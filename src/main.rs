4597618a11291af6942f1d806ec539177837a1b3
goat2130 <yagisawahodaka@team-lab.com>
2025-04-04 17:07:09 +0900

    udpate

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,7 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[unsafe(no_mangle)] // この関数の名前修飾をしない
 pub extern "C" fn _start() -> ! {
-    // リンカはデフォルトで `_start` という名前の関数を探すので、
-    // この関数がエントリポイントとなる
-    // let vga_buffer = 0xb8000 as *mut u8;
-
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         // VGA バッファは 80x25 のテキストモードのため、2バイトごとに
-    //         // 1バイト目が文字、2バイト目が属性を表す
-    //         // 0xb8000 は VGA バッファのアドレス
-    //         // 0xb8000 + i * 2 は i 番目の文字のアドレス
-    //         // 0xb8000 + i * 2 + 1 は i 番目の属性のアドレス
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
-
-    // use core::fmt::Write;
-    // vga_buffer::WRITER.lock().write_str("Hello again!\n").unwrap();
-    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-
     println!("Hello World{}", "!");
-
-    panic!("Some panic message");
     loop {}
 }
 
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,12 +1,13 @@
-use volatile::Volatile;
-use core::fmt;
-use lazy_static::lazy_static;
-use spin::Mutex;
+use volatile::Volatile; // メモリの読み書きを保証するための型
+use core::fmt; // フォーマット関連のトレイトをインポート
+use lazy_static::lazy_static; // 遅延初期化を行うためのマクロ
+use spin::Mutex; // スピンロックを提供するミューテックス
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Color {
+    // VGAテキストモードで使用可能な色を定義
     Black = 0,
     Blue = 1,
     Green = 2,
@@ -27,9 +28,10 @@ pub enum Color {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(transparent)]
-struct ColorCode (u8);
+struct ColorCode(u8); // 前景色と背景色を格納する構造体
 
 impl ColorCode {
+    // 新しいColorCodeを生成する関数
     fn new(foreground: Color, background: Color) -> ColorCode {
         ColorCode((background as u8) << 4 | (foreground as u8))
     }
@@ -38,41 +40,46 @@ impl ColorCode {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 struct ScreenChar {
-    ascii_character: u8,
-    color_code: ColorCode
+    // VGAバッファ内の1文字を表す構造体
+    ascii_character: u8, // ASCII文字
+    color_code: ColorCode, // 色コード
 }
 
-const BUFFER_HEIGHT: usize = 25;
-const BUFFER_WIDTH: usize = 80;
+const BUFFER_HEIGHT: usize = 25; // VGAバッファの高さ
+const BUFFER_WIDTH: usize = 80; // VGAバッファの幅
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    // VGAバッファ全体を表す構造体
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT], // 文字の2次元配列
 }
 
 pub struct Writer {
-    column_position: usize,
-    color_code: ColorCode,
-    buffer: &'static mut Buffer,
+    // 文字列を書き込むための構造体
+    column_position: usize, // 現在の列位置
+    color_code: ColorCode, // 使用する色コード
+    buffer: &'static mut Buffer, // VGAバッファへの参照
 }
 
 lazy_static! {
+    // グローバルなWriterインスタンスを定義
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+        color_code: ColorCode::new(Color::Yellow, Color::Black), // デフォルトの色設定
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) }, // VGAバッファのメモリアドレス
     });
 }
 
 impl Writer {
     pub fn write_byte(&mut self, byte: u8) {
+        // 1バイトを書き込む関数
         match byte {
-            b'\n' => self.new_line(),
+            b'\n' => self.new_line(), // 改行の場合は新しい行を作成
             byte => {
                 if self.column_position >= BUFFER_WIDTH {
-                    self.new_line();
+                    self.new_line(); // 行がいっぱいの場合は改行
                 }
-                let low = BUFFER_HEIGHT - 1;
+                let low = BUFFER_HEIGHT - 1; // 最下行
                 let col = self.column_position;
 
                 let color_code = self.color_code;
@@ -81,26 +88,28 @@ impl Writer {
                     color_code,
                 });
 
-                self.column_position += 1;
+                self.column_position += 1; // 列位置を更新
             }
         }
     }
 
     fn new_line(&mut self) {
+        // 新しい行を作成する関数
         for row in 1..BUFFER_HEIGHT {
             for col in 0..BUFFER_WIDTH {
                 let character = self.buffer.chars[row][col].read();
-                self.buffer.chars[row - 1][col].write(character);
+                self.buffer.chars[row - 1][col].write(character); // 上の行にコピー
             }
         }
 
-        self.clear_row(BUFFER_HEIGHT - 1);
-        self.column_position = 0;
+        self.clear_row(BUFFER_HEIGHT - 1); // 最下行をクリア
+        self.column_position = 0; // 列位置をリセット
     }
 
     fn clear_row(&mut self, row: usize) {
+        // 指定した行を空白でクリアする関数
         let blank = ScreenChar {
-            ascii_character: b' ',
+            ascii_character: b' ', // 空白文字
             color_code: self.color_code,
         };
         for col in 0..BUFFER_WIDTH {
@@ -111,19 +120,18 @@ impl Writer {
 
 impl Writer {
     pub fn write_string(&mut self, s: &str) {
+        // 文字列を書き込む関数
         for byte in s.bytes() {
             match byte {
-                // 出力可能なASCIIバイトか、改行コード
-                0x20..=0x7e | b'\n' => self.write_byte(byte),
-                // 出力可能なASCIIバイトではない
-                _ => self.write_byte(0xfe),
+                0x20..=0x7e | b'\n' => self.write_byte(byte), // 出力可能なASCIIバイトまたは改行
+                _ => self.write_byte(0xfe), // 出力不可能なバイトは「■」で代用
             }
-
         }
     }
 }
 
 impl fmt::Write for Writer {
+    // フォーマットされた文字列を書き込むためのトレイト実装
     fn write_str(&mut self, s: &str) -> fmt::Result {
         self.write_string(s);
         Ok(())
@@ -132,11 +140,13 @@ impl fmt::Write for Writer {
 
 #[macro_export]
 macro_rules! print {
+    // print!マクロの定義
     ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
 }
 
 #[macro_export]
 macro_rules! println {
+    // println!マクロの定義
     () => {
      ($crate::print!("\n"));   
     };
@@ -148,6 +158,7 @@ macro_rules! println {
 
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
+    // 実際にフォーマットされた文字列を出力する関数
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
