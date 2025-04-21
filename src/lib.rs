b9508fd3ad4c99107858059745b93448001a3244
goat2130 <yagisawahodaka@team-lab.com>
2025-04-21 17:57:18 +0900

    update: sec10

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
 
+extern crate alloc;
+
 use core::panic::PanicInfo;
 
 pub mod gdt;
@@ -12,6 +14,7 @@ pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 pub mod memory;
+pub mod allocator;
 
 pub fn init() {
     gdt::init();
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,19 @@
 #![test_runner(blog_os_goat::test_runner)] // テストランナーをカスタムのものに置き換える
 #![reexport_test_harness_main = "test_main"] // テストハーネスのメイン関数をカスタムのものに置き換える
 
+extern crate alloc; // アロケータを使用するために必要
+
 use core::panic::PanicInfo;
 use blog_os_goat::println; // println!マクロをインポート
 use bootloader::{BootInfo, entry_point}; 
+use alloc::{boxed::Box, rc::Rc, vec::Vec, vec}; // Boxを使用するために必要
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use blog_os_goat::memory::{self, BootInfoFrameAllocator};
     use x86_64::{VirtAddr, structures::paging::Page};
+    use blog_os_goat::allocator;
 
     println!("Hello World{}", "!");
     blog_os_goat::init();
@@ -21,8 +25,26 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization ");
+
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+
     // map an unused page
-        let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
 
     memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
